f468d8c2ba9aeea9bd8d2b7865793f89a84112f0
OberonSilni <sinadamov@gmail.com>
2022-09-03 01:56:11 +0200

    evo mene

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("de gas"),function(){let e=document.createElement("p");document.getElementById("content").appendChild(e),e.textContent="JALA BRAT KURCA MI PUSHI"}()})();
